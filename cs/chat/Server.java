4ed73c25081c39068ecaab1c1a3816e8942f6abd
firefly431 <firefly431.awesome@gmail.com>
2014-10-06 13:26:50 -0500

    Initial coding

--- cs/chat/Server.java
+++ cs/chat/Server.java
@@ -1,7 +1,23 @@
 package cs.chat;
 
+import java.io.*;
+import java.net.*;
+
 public class Server {
+    private static final int PORT = 6429;
+    private ServerSocket ss;
+
     public static void main(String argv[]) {
-        // initialize server
+        new Server().listen(Server.PORT);
+    }
+
+    public void listen(int port) {
+        try {
+            ss = new ServerSocket(port);
+        } catch (IOException e) {
+            System.err.println("Error initializing:");
+            e.printStackTrace();
+            System.exit(1);
+        }
     }
 }
