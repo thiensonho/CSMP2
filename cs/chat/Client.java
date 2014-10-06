cfc116f9739b6f801c7cb21d17c2373e24d6cbca
thiensonho <thiensonho@gmail.com>
2014-10-06 13:33:44 -0500

    nothihg
    f

--- cs/chat/Client.java
+++ cs/chat/Client.java
@@ -4,6 +4,9 @@ import java.io.*;
 import java.net.*;
 
 public class Client {
+
+    public static final int PORT = Server.PORT;
+
     public static void main(String argv[]) {
         // connect to server
         
