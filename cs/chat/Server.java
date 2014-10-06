24619731ae0ad9c5b77dcda26fb8593b0bea280c
firefly431 <firefly431.awesome@gmail.com>
2014-10-06 13:32:07 -0500

    Make server listen

--- cs/chat/Server.java
+++ cs/chat/Server.java
@@ -2,9 +2,12 @@ package cs.chat;
 
 import java.io.*;
 import java.net.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Server {
-    private static final int PORT = 6429;
+    public static final int PORT = 6429;
+    public static final int BACKLOG = 5;
     private ServerSocket ss;
 
     public static void main(String argv[]) {
@@ -13,11 +16,17 @@ public class Server {
 
     public void listen(int port) {
         try {
-            ss = new ServerSocket(port);
+            ss = new ServerSocket(port, BACKLOG);
         } catch (IOException e) {
             System.err.println("Error initializing:");
             e.printStackTrace();
             System.exit(1);
+        }
+        Logger.getLogger("server").info("Listening on " + ss.getInetAddress().toString() + ":" + ss.getLocalPort());
+        try {
+            ss.close();
+        } catch (IOException ex) {
+            Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
 }
