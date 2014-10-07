4d056bd89dbf63712a93548340aaaa5490e5d1e6
thiensonho <thiensonho@gmail.com>
2014-10-07 13:34:12 -0500

    fixed client blah blah

--- cs/chat/Client.java
+++ cs/chat/Client.java
@@ -9,6 +9,19 @@ public class Client {
 
     public static void main(String argv[]) {
         // connect to server
-        
+        try {
+            Socket cSocket = new Socket("10.21.50.118", PORT);
+            PrintWriter out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(cSocket.getOutputStream())));
+            BufferedReader in = new BufferedReader(new InputStreamReader(cSocket.getInputStream()));
+            out.println("CONNECT ThienSon");
+            out.flush();
+            String s = in.readLine();
+            System.out.println(s);
+            cSocket.close();
+        }
+        catch(IOException e)
+        {
+            e.printStackTrace();
+        }
     }
 }
