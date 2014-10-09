344ab2cf176f90e76942c4e5e1c1de8bb9bb4cde
thiensonho <thiensonho@gmail.com>
2014-10-09 13:39:00 -0500

    fixed client

--- cs/chat/Client.java
+++ cs/chat/Client.java
@@ -2,26 +2,84 @@ package cs.chat;
 
 import java.io.*;
 import java.net.*;
+import java.util.*;
 
 public class Client {
 
+    PrintWriter out;
+    BufferedReader in;
+    Socket cSocket;
+
     public static final int PORT = Server.PORT;
 
-    public static void main(String argv[]) {
+    public Client() {
+        out = null;
+        in = null;
+        cSocket = null;
+    }
+
+    public void run() {
         // connect to server
         try {
-            Socket cSocket = new Socket("10.21.50.118", PORT);
-            PrintWriter out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(cSocket.getOutputStream())));
-            BufferedReader in = new BufferedReader(new InputStreamReader(cSocket.getInputStream()));
-            out.println("CONNECT ThienSon");
+            Scanner scan = new Scanner(System.in);
+            cSocket = new Socket("10.21.50.118", PORT);
+            out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(cSocket.getOutputStream())));
+            in = new BufferedReader(new InputStreamReader(cSocket.getInputStream()));
+            System.out.println("Enter your name"); //prompt
+            out.println("CONNECT " + scan.nextLine());
             out.flush();
             String s = in.readLine();
             System.out.println(s);
-            cSocket.close();
+
         }
         catch(IOException e)
         {
             e.printStackTrace();
         }
+        // message thread
+        Thread messageThread = new Thread() {
+            @Override
+            public void run() {
+                // read input from console
+                // and then write "MESSAGE [input]" to the socket
+                // if input is /quit
+                // then write "QUIT" to the socket
+                Scanner scan = new Scanner(System.in);
+                while (true) {
+                    String input = scan.nextLine();
+                    if(input.equalsIgnoreCase("/quit"))
+                    {
+                       out.println("QUIT");
+                       break;
+                    }
+                    out.println("MESSAGE " + input);
+                    out.flush();
+                }
+                try {
+                    cSocket.close();
+                }
+                catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        messageThread.start();
+        do {
+            // read from in
+            // and print to System.out
+            try {
+                String input = in.readLine();
+                if(input == null) break;
+                System.out.println("MESSAGE" + input);
+            }
+            catch(IOException e) {
+                e.printStackTrace();
+                break;
+            }
+        } while(true);
+    }
+
+    public static void main(String argv[]) {
+        new Client().run();
     }
 }
